4d31efc9e268802b06944fe64de78dab1449c21e
FilipeDSADS <filipeaugustodsds@gmail.com>
2024-11-19 16:48:45 -0300

    Add files via upload

---
+++ senha.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<form action="/senha" method="GET" >
+	Contra Senha: <input type="text" name="senha" />
+	              <input type="submit"  value="Enviar"/>
+</form>
+</body>
+</html>
